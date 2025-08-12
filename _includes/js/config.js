986e30dec246546d2b24784e7466e052af140b45
Morten Høfft <6759267+MortenHofft@users.noreply.github.com>
2025-08-12 14:45:42 +0200

    init

--- _includes/js/config.js
+++ _includes/js/config.js
@@ -13,35 +13,17 @@ var siteConfig = {
       "id": "occurrenceKey"
     },
     {
-      "id": "collectionSearch"
-    },
-    {
-      "id": "collectionKey"
-    },
-    {
       "id": "datasetSearch"
     },
     {
       "id": "datasetKey"
-    },
-    {
-      "id": "institutionSearch"
-    },
-    {
-      "id": "institutionKey"
-    },
-    {
-      "id": "literatureSearch"
     }
   ],
   "disableInlineTableFilterButtons": false,
   "availableCatalogues": [
     // TODO: you should remove types you do not want to use
     "OCCURRENCE",
-    "DATASET",
-    "COLLECTION",
-    "INSTITUTION",
-    "LITERATURE"
+    "DATASET"
   ],
   "dataHeader": {
     "enableApiPopup": false,
@@ -91,13 +73,9 @@ var siteConfig = {
   "messages": {},
   "occurrenceSearch": {
     "scope": {
-      "type": "in",
-      "key": "publishingOrg",
-      "values": [
-        "760d5f24-4c04-40da-9646-1b2c935da502",
-        "2e7df380-8356-4533-bcb3-5459e23c794e",
-        "8e1a97a0-3ca8-11d9-8439-b8a03c50a862"
-      ]
+      "type": "equals",
+      "key": "protocol",
+      "value": "CAMTRAP_DP"
     },
     "highlightedFilters": [
       "taxonKey",
@@ -141,47 +119,14 @@ var siteConfig = {
       "zoom": 4.911544076366507
     }
   },
-  "collectionSearch": {
-    excludedFilters: ['country', 'active'],
-    // highlightedFilters: ['q', 'type', 'publishingOrg', 'license'],
-    // defaultTableColumns: ['title', 'description', 'publisher', 'type', 'occurrenceCount', 'literatureCount'],
-    scope: {
-      // TODO: you should add a scope here if you need search to be limited to a subset
-      // search filters have the format {field: [values]}
-      active: true
-    },
-  },
-  "institutionSearch": {
-    // excludedFilters: ['country', 'active'],
-    // highlightedFilters: ['q', 'type'],
-    // defaultTableColumns: ['title', 'type'],
-    scope: {
-      // TODO: you should add a scope here if you need search to be limited to a subset
-      // search filters have the format {field: [values]}
-      active: true
-    }
-  },
   "datasetSearch": {
     excludedFilters: ['publishingCountry', 'networkKey', 'projectId', 'hostingOrg'],
     highlightedFilters: ['q', 'type', 'publishingOrg', 'license'],
     scope: {
       // TODO: you should add a scope here if you need search to be limited to a subset
       // search filters have the format {field: [values]}
-      type: ['OCCURRENCE', 'CHECKLIST']
+      endpointType: ['CAMTRAP_DP']
     },
   },
   "publisherSearch": {},
-  "literatureSearch": {
-    "scope": {
-      // TODO: you should add a scope here if you need search to be limited to a subset
-      // literature search use the predicate format similar to occurrence search
-      "type": "in",
-      "key": "publishingOrganizationKey",
-      "values": [
-        "760d5f24-4c04-40da-9646-1b2c935da502",
-        "2e7df380-8356-4533-bcb3-5459e23c794e",
-        "8e1a97a0-3ca8-11d9-8439-b8a03c50a862"
-      ]
-    }
-  }
 }
